d446b51698f0bbb38ff007a3526c8a2ca7caf12d
Paweł Szumański <pawel.szumanski@outlook.com>
2024-10-26 17:25:21 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -14,6 +14,6 @@ function makeTransaction(quantity, pricePerDroid) {
     return `You ordered ${quantity} ${droid} worth ${totalPrice} credits!`;
 }
 
-console.log(makeTransaction(-5, 3000)); // "You ordered 5 droids worth 15000credits!" 
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000credits!" 
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!" 
 console.log(makeTransaction(10, 500)); // "You ordered10 droids worth 5000 credits!"
